ea068f6596be90589dbed12d8b986d546b3199d7
German Yunik <gpy1174@gmail.com>
2025-05-08 12:49:29 -0300

    Agregar archivos de proyecto.

---
+++ CRUDProducto/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using CRUDProducto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDProducto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            admProducto adm = new admProducto();
+            return View(adm.TraerProductos());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            Producto producto = new Producto
+            {
+                Nombre = collection["Nombre"].ToString(),
+                Descripcion = collection["Descripcion"].ToString(),
+                Precio = decimal.Parse(collection["Precio"].ToString()),
+                Stock = int.Parse(collection["Stock"].ToString()),
+            };
+
+            admProducto adm = new admProducto();
+            adm.CrearProducto(producto);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            admProducto adm = new admProducto();
+            return View(adm.MostrarProducto(Id));
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection collection)
+        {
+            Producto producto = new Producto
+            {
+                Id = int.Parse(collection["Id"].ToString()),
+                Nombre = collection["Nombre"].ToString(),
+                Descripcion = collection["Descripcion"].ToString(),
+                Precio = decimal.Parse(collection["Precio"].ToString()),
+                Stock = int.Parse(collection["Stock"].ToString()),
+
+            };
+            admProducto adm = new admProducto();
+            adm.EditarProducto(producto);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int Id)
+        {
+            admProducto adm = new admProducto();
+            return View(adm.MostrarProducto(Id));
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            admProducto adm = new admProducto();
+            return View(adm.MostrarProducto(Id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(FormCollection collection)
+        {
+            admProducto adm = new admProducto();
+            adm.EliminarProducto(int.Parse(collection["Id"].ToString()));
+            return RedirectToAction("Index");
+        }
+
+    } 
+}
---
+++ CRUDProducto/Models/admProducto.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace CRUDProducto.Models
+{
+	public class admProducto
+	{
+		SqlConnection conexion;
+		public void Conectar()
+		{
+			string stringConexion = ConfigurationManager.ConnectionStrings["Conexion"].ToString();
+			conexion = new SqlConnection(stringConexion);
+		}
+
+		public List<Producto> TraerProductos()
+		{
+			Conectar();
+
+			SqlCommand sentencia = new SqlCommand("select * from Productos", conexion);
+
+
+			conexion.Open();
+
+			List<Producto> productos = new List<Producto>();
+
+			SqlDataReader registros = sentencia.ExecuteReader();
+
+			while (registros.Read())
+			{
+				Producto producto = new Producto
+				{
+					Id = int.Parse(registros["Id"].ToString()),
+					Nombre = registros["Nombre"].ToString(),
+					Descripcion = registros["Descripcion"].ToString(),
+					Precio = decimal.Parse(registros["Precio"].ToString()),
+					Stock = int.Parse(registros["Stock"].ToString()),
+
+				};
+
+				productos.Add(producto);
+			}
+
+			return productos;
+
+		}
+
+		public int CrearProducto(Producto producto)
+		{
+			Conectar();
+
+			SqlCommand sentencia = new SqlCommand("insert into Productos (Nombre,Descripcion,Precio,Stock) values (@nombre,@descripcion,@precio,@stock)", conexion);
+
+			sentencia.Parameters.Add("@nombre", SqlDbType.Text);
+			sentencia.Parameters.Add("@descripcion", SqlDbType.Text);
+			sentencia.Parameters.Add("@precio", SqlDbType.Decimal);
+			sentencia.Parameters.Add("@stock", SqlDbType.Int);
+
+			sentencia.Parameters["@nombre"].Value = producto.Nombre;
+			sentencia.Parameters["@descripcion"].Value = producto.Descripcion;
+			sentencia.Parameters["@Precio"].Value = producto.Precio;
+			sentencia.Parameters["@Stock"].Value = producto.Stock;
+
+			conexion.Open();
+
+			int i = sentencia.ExecuteNonQuery();
+
+			conexion.Close();
+
+			return i;
+		}
+
+		public Producto MostrarProducto(int Id)
+		{
+			Conectar();
+
+			SqlCommand sentencia = new SqlCommand("select * from  Productos where Id=@id", conexion);
+
+			sentencia.Parameters.Add("@id", SqlDbType.Int);
+			sentencia.Parameters["@id"].Value = Id;
+			conexion.Open();
+			Producto producto = new Producto();
+			SqlDataReader registro = sentencia.ExecuteReader();
+			while (registro.Read())
+			{
+				producto.Id = Id;
+				producto.Nombre = registro["Nombre"].ToString();
+				producto.Descripcion = registro["Descripcion"].ToString();
+				producto.Precio = decimal.Parse(registro["Precio"].ToString());
+				producto.Stock = int.Parse(registro["Stock"].ToString());
+
+			}
+			conexion.Close();
+			return producto;
+		}
+
+		public int EditarProducto(Producto producto)
+		{
+			Conectar();
+
+			SqlCommand sentencia = new SqlCommand("update Productos set Nombre=@nombre,Descripcion=@descripcion,Precio=@precio,Stock=@stock where Id=@id", conexion);
+
+            sentencia.Parameters.Add("@Id", SqlDbType.Int);
+            sentencia.Parameters.Add("@nombre", SqlDbType.Text);
+			sentencia.Parameters.Add("@descripcion", SqlDbType.Text);
+			sentencia.Parameters.Add("@precio", SqlDbType.Decimal);
+			sentencia.Parameters.Add("@stock", SqlDbType.Int);
+
+			sentencia.Parameters["@nombre"].Value = producto.Nombre;
+			sentencia.Parameters["@descripcion"].Value = producto.Descripcion;
+			sentencia.Parameters["@Precio"].Value = producto.Precio;
+			sentencia.Parameters["@Stock"].Value = producto.Stock;
+            sentencia.Parameters["@Id"].Value = producto.Id;
+
+            conexion.Open();
+			int i = sentencia.ExecuteNonQuery();
+			conexion.Close();
+
+			return i;
+		}
+
+        public int EliminarProducto(int? Id)
+        {
+            Conectar();
+
+            SqlCommand sentencia = new SqlCommand("delete  from  Productos where Id=@id", conexion);
+
+            sentencia.Parameters.Add("@id", SqlDbType.Int);
+            sentencia.Parameters["@id"].Value = Id;
+            conexion.Open();
+            int i = sentencia.ExecuteNonQuery();
+            conexion.Close();
+
+			return i;
+        }
+    }
+}
